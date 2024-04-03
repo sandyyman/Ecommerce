020d0de7768738b4cb5075a50c4f29c8bae36e43
Naveen Gowda MY <117651142+naveengowda7@users.noreply.github.com>
2024-04-03 07:03:39 +0530

    Update Pagenotfound.js

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -6,7 +6,13 @@ import Layout from './../components/layout/Layout';
 //this page will be appeared in their window
 const Pagenotfound = () => {
     return (
-        <Layout><h1>Page not found </h1></Layout>
+        <Layout>
+            <div className='pnf'>
+                <h1 className='pnf-title'>404</h1>
+                <h2 className='pnf-heading'>Oops! Page Not Found</h2>
+                <Link to='/' className="pnf-btn">Go Back</Link>
+            </div>
+        </Layout>
     )
 }
 
