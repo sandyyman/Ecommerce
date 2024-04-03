61dacbb3b06ea4ae2e6a85805e92573312c2809b
Naveen Gowda MY <117651142+naveengowda7@users.noreply.github.com>
2024-04-03 07:03:03 +0530

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,7 +4,29 @@ import Layout from './../components/layout/Layout';
 //contact page
 const Contact = () => {
     return (
-        <Layout><h1>Contact Page</h1></Layout>
+       <Layout>
+            <div className='row contactus'>
+                <div className='col-md-6'>
+                    <img src="/images/contactus.jpg" alt="contactUs" style={{ width: "100%" }} />
+                </div>
+                <div className='col-md-4'>
+                    <h1 className='bg-dark p-2 text-white text-center'>Contact Us</h1>
+                    <p className='text-justify mt-2'>
+                        any query and info about product feel free to call anytime,
+                        we 24x7 available
+                    </p>
+                    <p className='mt-3'>
+                        <MdAttachEmail /> :help.InfinityCart@gmail.com
+                    </p>
+                    <p className='mt-3'>
+                        <FaPhoneSquareAlt /> : 080-0800-080
+                    </p>
+                    <p className='mt-3'>
+                        <MdOutlineSupportAgent /> : 080-0800-080
+                    </p>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
