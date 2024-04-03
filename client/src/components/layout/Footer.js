d76346b51f551a6c0a5be54c33c00fffbabece10
Naveen Gowda MY <117651142+naveengowda7@users.noreply.github.com>
2024-04-03 07:08:28 +0530

    Update Footer.js

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 //Footer Section for the WebPage
 const Footer = () => {
     return (
@@ -8,10 +8,18 @@ const Footer = () => {
                 already set in the public folder
                 index.html
                 */}
-            <div className='bg-dark text-light  p-3'>
-                <h4 className='text-center'>
+            <div className='footer'>
+                <h1 className='text-center'>
                     All Right Reserved &copy; InfinityCart
-                </h4>
+                </h1>
+                <p className='text-center mt-3'>
+                    <Link to='/about'>About</Link>
+                    |
+                    <Link to='/contact'>Contact</Link>
+                    |
+                    <Link to='/policy'>Privacy Policy</Link>
+
+                </p>
             </div>
         </>
     )
