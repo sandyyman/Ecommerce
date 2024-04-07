d765302c8a598864bf21136f4155b806b5f6e103
VivithRK <vivithkanchi25@gmail.com>
2024-04-07 12:54:35 +0530

    cart model product_id data changed to array

--- models/cart.js
+++ models/cart.js
@@ -1,26 +1,26 @@
 const mongoose = require("mongoose");
 
-
-const cartSchema = new mongoose.Schema({
+const cartSchema = new mongoose.Schema(
+  {
     user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    product_id: {
+    product_id: [
+      {
         type: String,
         required: true,
-    },
+      },
+    ],
     total_amount: {
-        type: String,
-        required: true,
-    }
-}
-    ,
-    {
-        timestamps: true
-    }
-
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Cart = mongoose.model("Cart", cartSchema);
