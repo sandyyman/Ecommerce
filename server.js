76c2c4dff1f9f5f9af7cfd1402be0e6dcba0ced1
sandyyman <127737652+sandyyman@users.noreply.github.com>
2024-04-11 20:11:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
-require("./config/db");
-const express = require("express");
+require('./config/db');
+const express = require('express');
+const mongoose = require('mongoose');
 const MongoStore = require("connect-mongo");
-const app = express();
-const session = require("express-session");
 const globalErrHandler = require("./middlewares/globalHandler");
-const port = process.env.PORT;
-const userRoutes = require("./routes/userRoute");
+const app = express();
+const session = require('express-session');
+const port = process.env.PORT
+const userRoutes = require('./routes/userRoute');
+const cartRoutes = require('./routes/cartRoute');
 const reviewRoutes = require("./routes/reviews");
 const productRoutes = require("./routes/product");
-const bodyParser = require("body-parser");
-const cartRoutes = require("./routes/cartRoute");
+const bodyParser = require('body-parser');
+const stripe = require('stripe')('');
 
 app.use(bodyParser.json());
 app.use(express.json());
 
-// *session
+
 const sessionSecret = process.env.SESSION_SECRET;
 app.use(
-  session({
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({
-      mongoUrl: process.env.MONGO_URL,
-      ttl: 24 * 60 * 60,
-    }),
-  })
+    session({
+        secret: sessionSecret,
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({
+            mongoUrl: process.env.MONGO_URL,
+            ttl: 24 * 60 * 60,
+        }),
+    })
 );
-
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
+app.use('cart', cartRoutes);
 app.use("/products", productRoutes);
 app.use("/reviews", reviewRoutes);
-app.use("/cart", cartRoutes);
 
-// *Error handler middleware
 app.use(globalErrHandler);
 
 app.listen(port, () => {
-  console.log("Server running");
-});
+    console.log("Server running");
+})
