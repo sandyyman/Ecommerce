dfe3d5d60c283bd91d62d0de1cdf8ab90224237c
sandyyman <127737652+sandyyman@users.noreply.github.com>
2024-04-20 19:32:19 +0530

    Add files via upload

--- models/cart.js
+++ models/cart.js
@@ -1,26 +1,23 @@
 const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema(
-  {
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
     },
-    product_id: [
-      {
-        type: String,
-        
-      },
-    ],
+    product: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "product",
+        required: true,
+    }],
     total_amount: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+        type: Number,
+        required: true,
+    }
+}, {
+    timestamps: true
+});
 
 const Cart = mongoose.model("Cart", cartSchema);
 module.exports = Cart;
